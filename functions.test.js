e9ef8db19208c1c481438197678d9cd03ccc5d68
Eliphaz <eliahlander3@gmail.com>
2022-03-14 15:01:24 -0600

    initial commit

---
+++ functions.test.js
@@ -0,0 +1,13 @@
+const { returnTwo, greeting, add } = require('./functions')
+
+test('test return 2',()=>{
+    expect(returnTwo()).toEqual(2)
+})
+
+test('test greeting',()=>{
+    expect(greeting('Eli')).toMatch(/hello/)
+})
+
+test('test add',()=>{
+    expect(add(1,2)).toEqual(3) && expect(add(5,9).toEqual(14))
+})
